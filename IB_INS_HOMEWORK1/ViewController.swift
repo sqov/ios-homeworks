3c99208d3f49f980d9f3230c51a2406523666bdf
iOS Warrior <ioswarrior@gmail.com>
2022-02-15 16:28:27 +0600

    Initial Commit

---
+++ IB_INS_HOMEWORK1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IB_INS_HOMEWORK1
+//
+//  Created by Alexander on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
