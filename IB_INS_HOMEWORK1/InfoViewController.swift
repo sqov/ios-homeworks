1598abfc453a4620a0e1abd8fb0c60984aea5189
iOS Warrior <ioswarrior@gmail.com>
2022-02-16 16:43:01 +0600

    1.3 Навигация в iOS и жизненный цикл UIViewController

---
+++ IB_INS_HOMEWORK1/InfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InfoViewController.swift
+//  IB_INS_HOMEWORK1
+//
+//  Created by Alexander on 16.02.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+}
