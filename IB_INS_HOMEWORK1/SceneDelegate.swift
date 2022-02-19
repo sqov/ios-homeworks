9eb5589b2ea109e6b847a1c6e8e88bf53ddf739f
iOS Warrior <ioswarrior@gmail.com>
2022-02-19 13:04:34 +0600

    Навигация в iOS и жизненный цикл UIViewController (программно)

--- IB_INS_HOMEWORK1/SceneDelegate.swift
+++ IB_INS_HOMEWORK1/SceneDelegate.swift
@@ -13,10 +13,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+       
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let profileVC = ProfileViewController()
+        let profileTabBarItem = UITabBarItem()
+        profileVC.title = "Profile"
+        profileVC.tabBarItem = profileTabBarItem
+        let navControllerProfile = UINavigationController(rootViewController: profileVC)
+        
+        let feedVC = FeedViewController()
+        let feedTabBarItem = UITabBarItem()
+        feedVC.title = "Feed"
+        feedVC.tabBarItem = feedTabBarItem
+        let navControllerFeed = UINavigationController(rootViewController: feedVC)
+        
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [navControllerProfile, navControllerFeed]
+        tabBarController.selectedViewController = navControllerProfile
+        
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
